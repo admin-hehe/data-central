7dfe1e97dfc8edd125b9dd0d2ddafda329c1865a
admin-hehe <reva.ardhanaph@gmail.com>
2026-04-03 15:25:25 +0800

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -1,1 +1,13 @@
+export async function onRequest(context) {
+  // Ambil URL GAS dari environment variable yang nanti kita set di dashboard
+  const gasUrl = context.env.GAS_URL;
 
+  // Forward request ke Google Apps Script
+  const response = await fetch(gasUrl, {
+    method: context.request.method,
+    headers: context.request.headers,
+    body: context.request.body
+  });
+
+  return response;
+}
